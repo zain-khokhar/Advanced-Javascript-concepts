0d907debb139973bbe62485f6b51fa2a4bd43452
zain-khokhar <zaink334705@gmail.com>
2026-01-02 14:46:05 +0500

    understanding with let

---
+++ basics/const.js
@@ -0,0 +1,9 @@
+// const property cannot be reassigned
+//  const a ; // ❌ SyntaxError: Missing initializer in const declaration
+const a = 10 ; // ✅  right syntax for initializing a const variable
+
+// reassigning a const variable
+
+// a = 20 ; // ❌ TypeError: Assignment to constant variable.
+
+console.log(a) ; // 10
