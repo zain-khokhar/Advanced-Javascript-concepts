59b4656e8a143fe79368e5b6460876680cc11ff6
zain-khokhar <zaink334705@gmail.com>
2026-01-04 08:42:31 +0500

    undersantding with destructing with nested objects

--- basics2/destructing.js
+++ basics2/destructing.js
@@ -1,9 +1,9 @@
 const arr = [1,2,3,4,5643,432,23,21,21,12,3,123,3];
 const  [a,b] = arr;
-console.log(a) // 1
-
+// console.log(a) // 1
+// use of rest operator with array 
 const [c,...restValues] = arr;
-console.log(restValues);
+// console.log(restValues);
 
 // rest operator with object 
 const person = {
@@ -14,4 +14,26 @@ const person = {
   profession: "developer"
 };
 const {name,...all}= person;
-console.log(all);
+// console.log(all);
+
+// destructing with nested objects
+
+const user = {
+  id: 1,
+  name: "Ali",
+  address: {
+    street: "Street 5",
+    one:{
+        work:"op"
+    },
+    city: "Karachi",
+    country: "Pakistan"
+  },
+  contact: {
+    email: "ali@example.com",
+    phone: "03001234567"
+  }
+};
+
+const {address:{one:{work}}} = user;
+console.log(work);
