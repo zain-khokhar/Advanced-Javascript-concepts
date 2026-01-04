fe94a5bf448b1fd4c4946429841dfd5f52508d5c
zain-khokhar <zaink334705@gmail.com>
2026-01-04 07:46:32 +0500

    undersantding with template literals

---
+++ basics2/template_literals.js
@@ -0,0 +1,22 @@
+const obj = {
+    id:"bc234",
+    name:"zain",
+    callName : ()=>{  
+    //    console.log(`this is my id ${obj.id} \` \` and this is \` my name ${obj.name} ` )
+    }
+}      
+
+obj.callName();
+
+// inject variables inside the another backticks
+function hello(name){
+ console.log(this);   
+    return name;
+}
+
+// console.log(hello("zain"));
+
+const Obj = {
+   name:`${console.log(this)}`
+}
+console.log(Obj.name)
