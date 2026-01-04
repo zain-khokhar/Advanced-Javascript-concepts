afadfdc47f20398acee8f08855647c3081714448
zain-khokhar <zaink334705@gmail.com>
2026-01-04 08:22:34 +0500

    undersantding with destructing of object and array

---
+++ basics2/destructing.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5643,432,23,21,21,12,3,123,3];
+const  [a,b] = arr;
+console.log(a) // 1
+
+const [c,...restValues] = arr;
+console.log(restValues);
+
+// rest operator with object 
+const person = {
+  name: "Ali",
+  age: 25,
+  gender: "male",
+  city: "Karachi",
+  profession: "developer"
+};
+const {name,...all}= person;
+console.log(all);
